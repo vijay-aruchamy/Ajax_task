99ced1cd3901f5069eedfb45893fc774f1e885be
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2023-09-05 15:40:36 +0530

    submission commit

--- ajax.js
+++ ajax.js
@@ -2,10 +2,10 @@ function loaddata() {
     let obj = new XMLHttpRequest();
     obj.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            if(!localStorage.getItem('responsekey'))
-            localStorage.setItem('responsekey', this.responseText);
+            if (!localStorage.getItem('responsekey'))
+                localStorage.setItem('responsekey', this.responseText);
 
-            
+
         }
     };
     obj.open("GET", "https://jsonplaceholder.typicode.com/todos/", true);
